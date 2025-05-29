772a5157848cfedbb93a02d970f9a6187d9b18f7
MeidaIvanauskaite <52979762+MeidaIvanauskaite@users.noreply.github.com>
2025-05-29 11:45:26 +0300

    Update PropertyMetadata.php

--- app/Models/PropertyMetadata.php
+++ app/Models/PropertyMetadata.php
@@ -3,5 +3,13 @@
     use Illuminate\Database\Eloquent\Model;
 
     class PropertyMetadata extends Model {
-        protected $fillable = ['property_id', 'tag', 'status', 'note'];
+        protected $fillable = ['google_property_id', 'tag', 'status', 'note', 'user_id'];
+
+        public function property() {
+            return $this->belongsTo(GoogleProperty::class, 'property_id', 'ga_property_id');
+        }
+
+        public function user() {
+            return $this->belongsTo(User::class);
+        }
     }
