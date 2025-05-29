d3edbc555757bb3d35a9010c77c77a53a9c0bbc3
MeidaIvanauskaite <52979762+MeidaIvanauskaite@users.noreply.github.com>
2025-05-29 11:45:08 +0300

    Update PageSpeedResult.php

--- app/Models/PageSpeedResult.php
+++ app/Models/PageSpeedResult.php
@@ -3,9 +3,13 @@
     use Illuminate\Database\Eloquent\Model;
 
     class PageSpeedResult extends Model {
-        protected $fillable = ['property_id', 'url', 'metrics', 'performance_score'];
+        protected $fillable = ['google_property_id', 'url', 'metrics', 'performance_score', 'user_id'];
 
         protected $casts = [
             'metrics' => 'array',
         ];
+
+        public function user() {
+            return $this->belongsTo(User::class);
+        }
     }
